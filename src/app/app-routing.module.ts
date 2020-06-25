f7060f8e79e2708dd667753abc6fc7d4502755de
Srdjan Milic <srdjanrmilic@gmail.com>
2020-06-25 21:54:26 +0200

    update: route settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoogleResultsComponent } from './components/google-results/google-results.component';
+import { BingResultsComponent } from './components/bing-results/bing-results.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home-page', pathMatch: 'full'},
+  { path: 'home-page', component: HomePageComponent },
+  { path: 'google-results', component: GoogleResultsComponent},
+  { path: 'bing-results', component: BingResultsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
